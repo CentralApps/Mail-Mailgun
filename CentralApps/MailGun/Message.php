eb7fa97869b182207025678e26abcd1207968575
Andy Tait <andy.s.tait@gmail.com>
2014-03-01 21:34:18 +0000

    Convert tabs to spaces. Standards fixes.

--- CentralApps/MailGun/Message.php
+++ CentralApps/MailGun/Message.php
@@ -1,68 +1,65 @@
 <?php
 namespace CentralApps\MailGun;
 
-class Message extends \CentralApps\Mail\Message {
-	
-	protected $tag = null;
-	
-	public function generateSendableArray()
-	{
-		$sendable = array();
-		$sendable['from'] = (string) $this->sender;
-		$sendable['subject'] = $this->subject;
-		$headers = $this->generateHeadersArray();
-		if( empty( $headers ) ) {
-			foreach($headers as $header) {
-				$sendable['h:' . $header->name ] = $header->value;
-			}
-		}
-		
-		if(count($this->attachments) > 0 ) {
-			$i=1;
-			foreach($this->attachments as $attachment) {
-				$sendable['attachment[' . $i . ']'] = '@' . $attachment->getPath() . '/' . $attachment->getFilename();
-			}
-		}
-		
-		
-		if( ! is_null($this->tag) ) {
-			$sendable['o:tag'] = $tag;
-		}
-		
-		if( ! is_null($this->replyTo) ) {
-			$sendable['h:Reply-To'] = (string) $this->replyTo;
-		}
-		
-		if( ! is_null($this->plainTextMessage) ) {
-			$sendable['text'] = $this->plainTextMessage;
-		}
-		
-		if( ! is_null($this->htmlMessage) ) {
-			$sendable['html'] = $this->htmlMessage;
-		}
-		
-		$sendable['to'] = implode(', ', $this->to->flattern() );
-		
-		if(count($this->bcc) > 0 ) {
-			$sendable['bcc'] = implode(', ' , $this->bcc->flattern() );
-		}
-		
-		if(count($this->cc) > 0 ) {
-			$sendable['cc'] = implode(', ', $this->cc->flattern() );
-		}
+class Message extends \CentralApps\Mail\Message
+{
+    protected $tag = null;
 
-		
-		return $sendable;
-		
-	}
-	
-	protected function generateHeadersArray()
-	{
-		$headers = array();
-		foreach( $this->headers as $header ) {
-			$headers[] = array( 'Name' => $header->name, 'Value' => $header->value );
-		}
-		return $headers;
-	}
-	
+    public function generateSendableArray()
+    {
+        $sendable = array();
+        $sendable['from'] = (string) $this->sender;
+        $sendable['subject'] = $this->subject;
+        $headers = $this->generateHeadersArray();
+        if (empty($headers)) {
+            foreach ($headers as $header) {
+                $sendable['h:' . $header->name ] = $header->value;
+            }
+        }
+
+        if (count($this->attachments) > 0) {
+            $i = 1;
+            foreach ($this->attachments as $attachment) {
+                $sendable['attachment[' . $i . ']'] = '@' . $attachment->getPath() . '/' . $attachment->getFilename();
+            }
+        }
+
+        if (!is_null($this->tag)) {
+            $sendable['o:tag'] = $tag;
+        }
+
+        if (!is_null($this->replyTo)) {
+            $sendable['h:Reply-To'] = (string) $this->replyTo;
+        }
+
+        if (!is_null($this->plainTextMessage)) {
+            $sendable['text'] = $this->plainTextMessage;
+        }
+
+        if (!is_null($this->htmlMessage)) {
+            $sendable['html'] = $this->htmlMessage;
+        }
+
+        $sendable['to'] = implode(', ', $this->to->flattern());
+
+        if (count($this->bcc) > 0) {
+            $sendable['bcc'] = implode(', ', $this->bcc->flattern());
+        }
+
+        if (count($this->cc) > 0) {
+            $sendable['cc'] = implode(', ', $this->cc->flattern());
+        }
+
+        return $sendable;
+    }
+
+    protected function generateHeadersArray()
+    {
+        $headers = array();
+        foreach ($this->headers as $header) {
+            $headers[] = array('Name' => $header->name, 'Value' => $header->value);
+        }
+
+        return $headers;
+    }
 }
